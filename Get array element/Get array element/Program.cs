c8a5c67f6124ce5e6ae1808ce46e58a09e3c0944
Taha Yasin Çiçek <116304917+tahayasincicek@users.noreply.github.com>
2025-03-11 23:32:07 +0300

    Add files via upload

---
+++ Get array element/Get array element/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = new int[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write("Enter numbers for index " + i + ":");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine("The numbers entered:");
+for(int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Index " + i + ": " + numbers[i]);
+}
---
+++ The biggest number/The biggest number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] numbers = { 21, 45, 38, 23, 59 };
+
+        
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i];
+            }
+        }
+
+        Console.WriteLine("The biggest number in the array is: " + max);
+    }
+}
